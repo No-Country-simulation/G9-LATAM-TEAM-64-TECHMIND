3ece5d94775b705641b85f39a92561e23aaec53f
Felix Aguilar Barrera <partcompe80@gmail.com>
2026-07-31 23:40:19 -0500

    agregar @CrossOrigin, cambiar getAllContenidos por getAllContents, retornar ResponseEntity

--- backend/src/main/java/com/techmind/backend/controller/ContenidoController.java
+++ backend/src/main/java/com/techmind/backend/controller/ContenidoController.java
@@ -1,15 +1,19 @@
 package com.techmind.backend.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static jdk.internal.org.jline.reader.impl.LineReaderImpl.CompletionType.List;
+
+@CrossOrigin(origins = "http://localhost:8080")
 @RequestMapping("/api/contenidos")
 @RestController
 public class ContenidoController {
     @GetMapping
-    public String getAllContenidos() {
-        return "Obteniendo todos los contenidos con una lista de contenidos";
+    public String getAllContents() {
+        return ResponseEntity<List<ContenidoResponseDTO>>;
     }
 
 }
--- backend/src/main/java/com/techmind/backend/repository/ContenidoRepository.java
+++ backend/src/main/java/com/techmind/backend/repository/ContenidoRepository.java
@@ -4,7 +4,10 @@ import com.techmind.backend.model.Contenido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ContenidoRepository extends JpaRepository<Contenido, Long> {
     // Aquí podemos añadir métodos de consulta personalizados si fuera necesario.
+    public List<Contenido> findAll();
 }
