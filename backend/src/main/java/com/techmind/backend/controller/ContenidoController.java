055fb967e9013e1145a8f87d1033c7c3d0b87774
Felix Aguilar Barrera <partcompe80@gmail.com>
2026-08-03 21:44:36 -0500

    merge con la rama develop

--- backend/src/main/java/com/techmind/backend/controller/ContenidoController.java
+++ backend/src/main/java/com/techmind/backend/controller/ContenidoController.java
@@ -1,6 +1,10 @@
 package com.techmind.backend.controller;
 
+import com.techmind.backend.dto.ContenidoResponseDTO;
 import com.techmind.backend.repository.ContenidoRepository;
+
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@CrossOrigin(origins = "http://localhost:aqui el puerto")
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/contenidos")
 @RestController
 public class ContenidoController {
--- backend/src/main/java/com/techmind/backend/repository/ContenidoRepository.java
+++ backend/src/main/java/com/techmind/backend/repository/ContenidoRepository.java
@@ -1,5 +1,6 @@
 package com.techmind.backend.repository;
 
+import com.techmind.backend.dto.ContenidoResponseDTO;
 import com.techmind.backend.model.Contenido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
